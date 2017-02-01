1577b63d2345be55e6f662d61355260167fb0eb8
Alberto Castillo Molina <acmxipiron14@gmail.com>
2017-02-02 00:28:46 +0100

    Logger añadido en App.java

--- Maven1/src/main/java/es/egc/app/App.java
+++ Maven1/src/main/java/es/egc/app/App.java
@@ -4,10 +4,13 @@ package es.egc.app;
  * Hello world!
  *
  */
+import org.apache.log4j.*; 
 public class App 
 {
     public static void main( String[] args )
     {
+		static Logger log=Logger.getLogger(App.class); 
+		log.info("Returning 1");
         System.out.println( "Hola mundo!! Mi nombre es Alberto Castillo Molina" );
     }
 }
